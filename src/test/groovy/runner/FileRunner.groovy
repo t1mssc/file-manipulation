80cc882c189bf592039d1c0bbb28d21975d4072e
CJ Castillo <caytejohntims@gmail.com>
2026-02-20 10:51:35 +0800

    Disabled the tags to run the multiple features

--- src/test/groovy/runner/FileRunner.groovy
+++ src/test/groovy/runner/FileRunner.groovy
@@ -6,14 +6,14 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber.class)
 @CucumberOptions (
-        features = "src/test/resources/features/",
+        features = "src/test/resources/features",
         glue = ["steps"],
         plugin = [
                 "pretty",
-                "html:src/test/resources/cucumber-reports/file-manipulation-report.html",
-                "json:src/test/resources/cucumber-reports/file-manipulation-report.json"
+                "html:src/test/resources/testing-reports/file-manipulation-report.html",
+                "json:src/test/resources/testing-reports/file-manipulation-report.json"
         ],
-        tags = "@intermediate and @beginner",
+        //tags = "@intermediate and @beginner",
         monochrome = true
 )
 class FileRunner {
