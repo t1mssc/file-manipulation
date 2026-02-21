56f5c089aa23c0790b57c5af8de2b38bbbef6f4d
CJ Castillo <caytejohntims@gmail.com>
2026-02-21 14:15:32 +0800

    Create helpers for updating the json file

---
+++ src/test/groovy/Helpers/Helpers.groovy
@@ -0,0 +1,37 @@
+package Helpers
+
+import groovy.json.JsonOutput
+
+class Helpers {
+
+    static def getNestedValue(Map jsonData, String path) {
+        List<String> keys = path.split('\\.') as List<String>
+        def current = jsonData
+        keys.each { key -> current = current[key] }
+        return current.toString()
+    }
+
+    static def updateNestedValue(Map map, String path, String value) {
+        List<String> keys = path.split('\\.') as List<String>
+        Map current = map
+
+        for (int i = 0; i < keys.size() - 1; i++) {
+            current = current[keys[i]] as Map
+        }
+        current[keys[keys.size() - 1]] = value
+    }
+
+    static def saveJsonFile(File jsonFile, Map jsonData) {
+        String json = JsonOutput.prettyPrint(JsonOutput.toJson(jsonData))
+        jsonFile.text = json
+        println "Saved JSON file: ${jsonFile.absolutePath}"
+    }
+
+    Map makeMutable(Map map) {
+        Map mutableMap = new HashMap(map)
+        mutableMap.each { key, val ->
+            if (val instanceof Map) mutableMap[key] = makeMutable(val as Map)
+        }
+        return mutableMap
+    }
+}
